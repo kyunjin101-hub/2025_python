f0960f7704ff20de18c548c8a5b9f42fafc26411
kyunjin101-hub <kyunjin101@gmail.com>
2025-09-25 10:50:58 +0900

    Add files via upload

---
+++ label2.py
@@ -0,0 +1,24 @@
+import tkinter as tk          
+root = tk.Tk()
+root.title("Tkinter 예제")
+root.geometry("200x100")
+
+label = tk.Label(root, text="Hello, Tkinter!")
+label.pack(pady=20)
+
+root.mainloop()
+
+from tkinter import*
+
+root = Tk()
+
+Label(root,
+      text = "Times Font 폰트와 빨강색을 사용한다.",
+      fg = "red",
+      font = "Times 32 bold italic").pack()
+Label(root,
+      text="Helvetica 폰트와 녹색을 사용한다.",
+      fg = "blue",
+      bg = "yellow", 
+      font = "Helvetica 32 bold italic").pack()
+root.mainloop()
