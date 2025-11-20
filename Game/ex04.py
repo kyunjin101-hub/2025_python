541a2654fabfdc0c2228a3c32dac2a89b1788f77
kyunjin101-hub <kyunjin101@gmail.com>
2025-11-20 11:34:33 +0900

    Add files via upload

---
+++ Game/ex04.py
@@ -0,0 +1,26 @@
+import turtle
+
+t = turtle.Pen()
+t.speed(10)
+
+def mystar(size,filled):
+    if filled:
+        t.begin_fill()
+
+    for z in range(1,19):
+        t.forward(100)
+        if z % 2 == 0:
+            t.left(175)
+        else:
+            t.left(225)
+    if filled:
+        t.end_fill()        
+
+t.color(0.9,0.75,0)
+mystar(120,True)
+
+t.color(0,0,0)
+mystar(120,False)
+
+turtle.done
+
