cbfa4ca0964cd842694a9420c6c88f3bc4035798
kyunjin101-hub <kyunjin101@gmail.com>
2025-12-12 15:07:20 +0900

    Add files via upload

---
+++ Game 과제/game_01.py
@@ -0,0 +1,113 @@
+from tkinter import *
+import random
+import time
+
+tk = Tk()
+tk.title("Game")
+tk.resizable(0,0)
+tk.wm_attributes("-topmost",1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+class Ball:
+    def __init__(self,canvas,paddle,color):
+        self.canvas=canvas
+        self.id=canvas.create_oval(10,10,25,25,fill=color)
+        self.paddle=paddle
+        self.canvas.move(self.id,245,100)
+
+        starts = [-3,-2,-1,1,2,3]
+        random.shuffle(starts)
+        self.x= starts[0]
+        self.y=-3
+
+        self.canvas_height = self.canvas.winfo_height()    
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.hit_bottom = False
+
+    def hit_paddle(self,pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+    
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y)
+        pos=self.canvas.coords(self.id)
+
+        if pos[1] <= 0:
+            self.y = 3
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        
+        if not self.hit_bottom and self.hit_paddle(pos):
+            self.y = -3
+
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas=canvas
+        self.id=canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.id,200,300)
+
+        self.x=0
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.canvas.bind_all("<KeyPress-Left>",self.turn_left)
+        self.canvas.bind_all("<KeyPress-Right>",self.turn_right)
+
+    def turn_left(self,evt):
+        self.x=-2
+    
+    def turn_right(self,evt):
+        self.x = 2
+    
+    def draw(self):
+        self.canvas.move(self.id,self.x,0)
+        pos = self.canvas.coords(self.id)
+
+        if pos[0] <= 0:
+            self.x = 0
+
+        if pos[2] > self.canvas_width:
+            self.x = 0
+
+
+def start_game():
+    canvas.delete("all") 
+
+    paddle = Paddle(canvas, "blue")
+    ball = Ball(canvas, paddle, "red")
+
+    game_over_text_id = None
+
+    while True:
+        if not ball.hit_bottom:
+            ball.draw()
+            paddle.draw()
+        else:
+            if game_over_text_id is None:
+                game_over_text_id = canvas.create_text(
+                    250, 200, text="GAME OVER", font=("Arial", 24), fill="red"
+                )
+                tk.update()
+                time.sleep(1)
+                start_game() 
+                return
+
+        tk.update_idletasks()
+        tk.update()
+        time.sleep(0.01)
+
+
+start_game()
+tk.mainloop()
---
+++ Game 과제/game_03.py
@@ -0,0 +1,118 @@
+from tkinter import *
+import random
+import time
+import pygame
+
+tk = Tk()
+tk.title("Game")
+tk.resizable(0,0)
+tk.wm_attributes("-topmost",1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+tk.update()
+
+#소리 효과 추가
+pygame.mixer.init()
+hit_sound = pygame.mixer.Sound("super-mario-coin-sound.mp3") 
+
+class Ball:
+    def __init__(self,canvas,paddle,color,score_obj):
+        self.canvas=canvas
+        self.id=canvas.create_oval(10,10,25,25,fill=color)
+        self.paddle=paddle
+        self.canvas.move(self.id,245,100)
+
+        starts = [-3,-2,-1,1,2,3]
+        random.shuffle(starts)
+        self.x= starts[0]
+        self.y=-3
+
+        self.canvas_height = self.canvas.winfo_height()    
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.hit_bottom = False
+        
+        self.score = score_obj
+
+    def hit_paddle(self,pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
+    
+    def draw(self):
+        self.canvas.move(self.id,self.x,self.y)
+        pos=self.canvas.coords(self.id)
+
+        if pos[1] <= 0:
+            self.y = 3
+
+        if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        
+        if not self.hit_bottom:
+            if self.hit_paddle(pos):
+                #소리효과
+                hit_sound.play()
+                self.y = -3
+                self.score.add_point()
+
+        if pos[0] <= 0:
+            self.x = 3
+        if pos[2] >= self.canvas_width:
+            self.x = -3
+
+class Paddle:
+    def __init__(self,canvas,color):
+        self.canvas=canvas
+        self.id=canvas.create_rectangle(0,0,100,10,fill=color)
+        self.canvas.move(self.id,200,300)
+
+        self.x=0
+        self.canvas_width = self.canvas.winfo_width()
+
+        self.canvas.bind_all("<KeyPress-Left>",self.turn_left)
+        self.canvas.bind_all("<KeyPress-Right>",self.turn_right)
+    
+    def turn_left(self,evt):
+        self.x=-2
+    
+    def turn_right(self,evt):
+        self.x = 2
+    
+    def draw(self):
+        self.canvas.move(self.id,self.x,0)
+        pos = self.canvas.coords(self.id)
+
+        if pos[0] <= 0:
+            self.x = 0
+
+        if pos[2] > self.canvas_width:
+            self.x = 0
+
+class Score:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.score = 0
+        self.id = canvas.create_text(450, 20, text="0", font=("Arial", 20), fill="black")
+
+    def add_point(self):
+        self.score += 1
+        self.canvas.itemconfig(self.id, text=str(self.score))
+
+score = Score(canvas)
+
+paddle = Paddle(canvas,"blue")
+ball = Ball(canvas,paddle,"red",score)
+
+
+while True:
+    if ball.hit_bottom == False:
+        ball.draw()
+        paddle.draw()
+    tk.update_idletasks() 
+    tk.update() 
+    time.sleep(0.01)
